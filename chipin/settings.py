ba0af998a8719f42f6d917764c37c8324ee06983
ninjaaung <ninja.aung02@gmail.com>
2020-03-04 09:48:07 -0800

    Setting Collect Static to None

--- chipin/settings.py
+++ chipin/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = None
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
